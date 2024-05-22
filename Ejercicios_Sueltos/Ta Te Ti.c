b1a5df5ba79f2979ded61f9aada70533509aa62b
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-05-22 12:10:33 -0300

    Create Ta Te Ti.c

---
+++ Ejercicios_Sueltos/Ta Te Ti.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char * argv[]){
+    srand(time(NULL));
+
+    int turno = (rand()% (2)) + 1;
+
+    printf("El jugador 1 es la 'O' y el jugador 2 es la 'X\n\n");
+    char tablero [3][3];
+    
+    printf("Comienza el jugador %d\n", turno);
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            tablero [i][j] = '*';
+            printf("%c ", tablero[i][j]);
+        }
+        printf("\n");
+    }
+
+    int intentos=1;
+
+    while(intentos <= 9){
+        int posicion1=0;
+        int posicion2=0;
+
+
+        if(turno == 1){
+            printf("(Jugador 1) Seleccione la fila: ");
+            scanf("%d", &posicion1);
+            printf("(Jugador 1) Seleccione la columna: ");
+            scanf("%d", &posicion2);
+            if(tablero [posicion1-1][posicion2-1] = 'X'){
+                printf("Perdes el turno por equivocarte");
+                break;
+            }
+            tablero [posicion1-1][posicion2-1] = 'O';
+            printf("\n");
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    printf("%c ", tablero[i][j]);
+                }
+                printf("\n");
+            }
+            
+            turno++;
+            intentos++;
+        }
+
+        if(turno == 2){
+            printf("(Jugador 2) Seleccione la fila: ");
+            scanf("%d", &posicion1);
+            printf("(Jugador 2) Seleccione la columna: ");
+            scanf("%d", &posicion2);
+            tablero [posicion1-1][posicion2-1] = 'X';
+            printf("\n");
+            if(tablero [posicion1-1][posicion2-1] = 'O'){
+                printf("Perdes el turno por equivocarte");
+                break;
+            }
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    printf("%c ", tablero[i][j]);
+                }
+            printf("\n");
+            }
+            turno--;
+            intentos++;
+        }
+    }
+}
