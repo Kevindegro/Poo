f8af086cc655258806373b9d63f21b5d84711575
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-06-25 11:09:42 -0600

    Update Prueba 2_Tema  4.c

--- Prueba 2_Tema  4.c
+++ Prueba 2_Tema  4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+int main(){
+    srand(time(NULL));
+    int N=15;
+    int a=srand() % 15;
+    char tablero[N][N];
 
+    for(int i=0;i<=N;i++){
+        for(int j=0;j<=N;j++){
+            tablero[i][j]='X';
+            printf("%c", tablero[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+
+    return 0;
+}
