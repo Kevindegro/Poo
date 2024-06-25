a958d6b19c8395becc30316bc9b0c47879d2847a
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-06-25 11:53:05 -0600

    Update Prueba 2_Tema  4.c

--- Prueba 2_Tema  4.c
+++ Prueba 2_Tema  4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+//El valor 3 se usa para mostrar que ambos jugadores estan en la misma posicion
 //Tortuga = 1.
 //Liebre = 2.
 //Pozo == O = Pierde una vida.
@@ -10,7 +10,12 @@
 
 int main(){
     srand(time(NULL));
+    int L1=0;
+    int L2=0;
+    int T1=0;
+    int T2=0;
     int N=14;
+    int dado;
     int a;
     int b;
     int vidas_T=3;
@@ -22,7 +27,7 @@ int main(){
     printf("1-Equipo Tortuga\n2-Equipo Libre\n\n");
     scanf("%d", &Equipo);
 
-    
+
 //Llena la matriz con X
     for(int i=0;i<=N;i++){
         for(int j=0;j<=N;j++){
@@ -63,6 +68,25 @@ int contador2 = 0;
     }
 
 
+
+//bucle para relizar los movimientos y jugadas/elecciones.
+    int bucle = 10;
+    while(bucle>5){
+        int opciones;
+        tablero[0][0] = 3:
+        if(Equipo== 1){
+            printf("Elija una opcion:\n 1-Tirar dado.\n 2-Usar Comodin.\n 3-Rendirse.\n\n");
+            scanf("%d", &opciones);
+
+            if(opciones == 1){
+                dado = rand("")
+            }
+        }
+
+        if(Equipo == 2){
+
+        }
+    }
 
     return 0;
 }
