abcd229d02546b013270d896d5e22dce48260564
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-06-25 11:38:01 -0600

    Update Prueba 2_Tema  4.c

--- Prueba 2_Tema  4.c
+++ Prueba 2_Tema  4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 
@@ -10,26 +11,27 @@
 int main(){
     srand(time(NULL));
     int N=14;
-    int a=0;
-    int b=0;
-
+    int a;
+    int b;
+    int vidas_T=3;
+    int vidas_L=3;
     char tablero[N][N];
 
-//Coloca la 'X' en la matriz
+    printf("Bienvenido al juego de la Liebre y la tortuga.\n             Seleccione un equipo:\n\n");
+
+//Llena la matriz con X
     for(int i=0;i<=N;i++){
         for(int j=0;j<=N;j++){
             tablero[i][j]='X';
-            printf("%c ", tablero[i][j]);
         }
-        printf("\n");
     }
 
 
 //Coloca de forma aleatoria la O
     int contador1 = 0;
     while(contador1 !=2){
-        a = rand();
-        b = rand();
+        a = rand () % 14;
+        b = rand() % 14;
         if(tablero[a][b]== 'X'){
         tablero [a][b] = 'C';
         contador1++;
@@ -40,13 +42,22 @@ int main(){
 //Coloca de forma aleatoria la C
 int contador2 = 0;
     while(contador2 !=2){
-        a = rand();
-        b = rand();
-        if(tablero[a][b]== 'X'){
+        a = rand () % 14;
+        b = rand() % 14;
+        if(tablero[a][b] == 'X'){
         tablero [a][b] = 'O';
         contador2++;
         }
     }
+
+//Imprime la matriz
+   for(int i=0;i<=N;i++){
+        for(int j=0;j<=N;j++){
+            printf("%c ", tablero[i][j]);
+        }
+        printf("\n");
+    }
+
 
 
     return 0;
