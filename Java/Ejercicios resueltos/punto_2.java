9d021d5312e781c3811e73032d948b4f2e2eea5b
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-09-03 14:29:45 -0300

    Create punto_2.java

---
+++ Java/Ejercicios resueltos/punto_2.java
@@ -0,0 +1,17 @@
+
+public class punto_2 {
+	
+	public static void main(String[] args) {
+		int a= 2;
+		int b=3;
+		
+		if(a>b) {
+			System.out.println("El numero " +(a)+" es mayor");
+		}
+		else
+		if(a<b) {
+			System.out.println("El numero " +(b)+" es mayor");
+		}
+
+	}
+}
