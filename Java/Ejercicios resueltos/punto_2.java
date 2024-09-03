62f755f9474b73be5c5f6dc61ef641c80899d9a7
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-09-03 14:32:58 -0300

    Update punto_2.java

--- Java/Ejercicios resueltos/punto_2.java
+++ Java/Ejercicios resueltos/punto_2.java
@@ -2,15 +2,17 @@
 public class punto_2 {
 	
 	public static void main(String[] args) {
-		int a= 2;
+		int a= 3;
 		int b=3;
 		
 		if(a>b) {
 			System.out.println("El numero " +(a)+" es mayor");
 		}
-		else
 		if(a<b) {
 			System.out.println("El numero " +(b)+" es mayor");
+		}
+		if(a==b) {
+			System.out.println("Son iguales");
 		}
 
 	}
