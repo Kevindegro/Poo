2ea110eecf342a89c7032f9f6ba8effa64e8aaf9
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-09-03 14:43:49 -0300

    Update punto_3.java

--- Java/Ejercicios resueltos/punto_3.java
+++ Java/Ejercicios resueltos/punto_3.java
@@ -4,6 +4,6 @@ public class punto_3 {
 	public static void main(String[] args) {
 		String nombre = "Kevin";
 		
-		System.out.println("Bienbenido "+(nombre));
+		System.out.println("Bienvenido "+(nombre));
 	}
 }
