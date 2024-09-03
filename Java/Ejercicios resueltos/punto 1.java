d7d72a682ace42012669e3d1c99ccac37bf53053
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-09-03 14:16:23 -0300

    Create punto 1.java

---
+++ Java/Ejercicios resueltos/punto 1.java
@@ -0,0 +1,14 @@
+
+public class punto_1 {
+	
+	public static void main(String[] args) {
+		int num1= 6;
+		int num2=3;
+		
+		System.out.println("Suma "+(num1+num2));
+		System.out.println("Resta "+(num1-num2));
+		System.out.println("Suma "+(num1*num2));
+		System.out.println("Suma "+(num1/num2));
+
+	}
+}
