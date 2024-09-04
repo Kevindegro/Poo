aa3b61173849529de9d26297468c6a602cf720d9
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-09-04 11:52:20 -0300

    Create punto_5.java

---
+++ Java/Ejercicios resueltos/punto_5.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class punto_5 {
+
+	public static void main(String[] args) {
+
+		String texto=JOptionPane.showInputDialog("Introduce un radio");
+		double radio=Double.parseDouble(texto);
+		double area=Math.PI*Math.pow(radio, 2);
+		System.out.println("El area del circulo es "+area);
+    
+	}
+}
