6699d57cef8218709ce4f1a1cc25051e186741bf
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:24:02 -0300

    Create punto_31.java

---
+++ Java/Ejercicios resueltos/punto_31.java
@@ -0,0 +1,9 @@
+public class punt_31 {
+
+    public static void main(String[] args) {
+        String cadena="Hola mundo";
+        System.out.println("La cadena tiene "+cadena.length()+" caracteres");
+        
+    }
+    
+}
