0dddbc91f96f712c2713541d802b93b5ab0b0d5b
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:27:39 -0300

    Create punto_33.java

---
+++ Java/Ejercicios resueltos/punto_33.java
@@ -0,0 +1,9 @@
+public class punto_33 {
+
+    public static void main(String[] args) {
+      
+        String cadena= "Hola mundo";
+        String subcadena = cadena.substring(3, 5);
+        System.out.println(subcadena);
+    }
+}
