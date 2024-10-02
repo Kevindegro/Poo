ee1ea68dc04f8433adb6ecea945ac65bad717d37
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:15:26 -0300

    Create punto_24.java

---
+++ Java/Ejercicios resueltos/punto_24.java
@@ -0,0 +1,11 @@
+public class punto_24 {
+
+	public static void main(String[] args) {
+
+		String cadena="Ayuda profe no se que poner v2.0";
+
+		for (int i=0;i<cadena.length();i++){
+			System.out.print((int)cadena.charAt(i)+" ");
+		}
+	}
+}
