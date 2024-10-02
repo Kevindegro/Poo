93d36a185a2fba4af758051354496961c2eb8b04
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:25:19 -0300

    Create punto_32.java

---
+++ Java/Ejercicios resueltos/punto_32.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class punto_32 {
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        
+        System.out.println("Escribe la palabra 1");
+        String palabra1 = sn.next();
+        System.out.println("Escribe la palabra 2");
+        String palabra2 = sn.next();
+        
+        if(palabra1.equals(palabra2)){
+            System.out.println("Las palabras son iguales");
+        }else{
+            System.out.println("Las palabras no son iguales");
+        }
+        
+    }
