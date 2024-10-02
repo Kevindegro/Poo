3a2347d386d47a320e8f3a27e41d74b0b66e2021
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:13:47 -0300

    Create punto_23.java

---
+++ Java/Ejercicios resueltos/punto_23.java
@@ -0,0 +1,9 @@
+public class punto_23 {
+
+	public static void main(String[] args) {
+
+		String cadena="Ayuda profe no se que tengo que poner ";
+		System.out.print(cadena.replace('a', 'e'));
+
+	}
+}
