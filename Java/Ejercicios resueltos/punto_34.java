4c8c3b3a665bc336184e6eeabcd560ddcb042709
Kevindegro <130598993+Kevindegro@users.noreply.github.com>
2024-10-02 16:28:19 -0300

    Create punto_34.java

---
+++ Java/Ejercicios resueltos/punto_34.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class punto_34 {
+
+    public static void main(String[] args) {
+     
+        Scanner sn = new Scanner(System.in);
+        sn.useDelimiter("\n");
+        System.out.println("Escribe una frase");
+        String frase=sn.next();
+        String palabras[] = frase.split(" ");
+      
+        for(int i=0;i<palabras.length;i++){
+            System.out.println(palabras[i]);
+        }
+    }
+}
